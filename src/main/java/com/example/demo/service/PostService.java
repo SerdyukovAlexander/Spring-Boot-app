a3dd7afb7e13e192f8f0836c19fc79f13ec78a76
Serdyukov Alexander <30432511+SerdyukovAlexander@users.noreply.github.com>
2025-02-21 15:52:00 +0500

    post_create

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -3,18 +3,27 @@ package com.example.demo.service;
 import com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService
 {
+    private final List<Post> posts = new ArrayList<>();
+    {
+        posts.add(new Post("Первый пост", new Date()));
+        posts.add(new Post("Второй пост", new Date()));
+        posts.add(new Post("Третий пост", new Date()));
+    }
+
     public List<Post> listAllPosts()
     {
-        return List.of(
-                new Post("Первый пост", new Date()),
-                new Post("Второй пост", new Date()),
-                new Post("Третий пост", new Date())
-        );
+        return new ArrayList<>(posts);
+    }
+
+    public void create(String text)
+    {
+        posts.add(new Post(text, new Date()));
     }
 }
